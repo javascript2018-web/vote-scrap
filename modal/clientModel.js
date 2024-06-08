070d0ad721b8602f8768338f6c1db35366a0295d
javascript2018-web <fiverrinbox59@gmail.com>
2024-06-08 15:17:19 +0600

    update

---
+++ modal/clientModel.js
@@ -0,0 +1,17 @@
+// clientModel.js
+const mongoose = require('mongoose');
+
+const clientSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    phone: String,
+    country: String,
+    city: String,
+    state: String,
+    group: String,
+    userEmail: String
+});
+
+const Client = mongoose.model('Client', clientSchema);
+
+module.exports = Client;
