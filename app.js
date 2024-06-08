6fe973a0bb8679baa50993fddc0ac66a072f7f16
javascript2018-web <fiverrinbox59@gmail.com>
2024-06-08 15:07:40 +0600

    update

--- app.js
+++ app.js
@@ -26,15 +26,6 @@ const clientSchema = new mongoose.Schema({
   userEmail: String
 
 });
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-api_key: process.env.CLOUDINARY_API_KEY,
-api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-
 
 const Client = mongoose.model('Client', clientSchema);
 
