6bc65e6f334762cbebaa09a9ca75d4ed4c77215e
javascript2018-web <fiverrinbox59@gmail.com>
2024-06-08 15:45:01 +0600

    update

--- app.js
+++ app.js
@@ -146,52 +146,8 @@ const sendWhatsapp = async (to, message) => {
   console.log('Sending WhatsApp message:', whatsapp);
   await client.messages.create(whatsapp);
 };
-app.post('/api/send_message', upload.single('attachment'), async (req, res) => {
-  res.header('Content-Type', 'application/json');
-  try {
-    const { subject, content, group, messageType } = req.body;
-    console.log('Initial Request Body:', req.body);
 
-    // Ensure empty group is handled
-    if (!group) {
-      throw new Error('Group (phone number or email) is required');
-    }
 
-    let attachmentUrl = '';
-    if (req.file) {
-      console.log('Uploading file to Cloudinary...');
-      const result = await cloudinary.uploader.upload(req.file.path);
-      attachmentUrl = result.secure_url;
-      console.log('File uploaded to Cloudinary:', attachmentUrl);
-    }
-
-    // Save the message to the database
-    const newMessage = new Message({
-      subject,
-      content,
-      group,
-      attachmentUrl,
-      messageType,
-    });
-
-    console.log('Saving message to database:', newMessage);
-    await newMessage.save();
-    console.log('Message saved to database');
-
-    // Send message based on type
-    if (messageType === 'email') {
-      await sendEmail(group, subject, content);
-    } else if (messageType === 'sms') {
-      await sendSms(subject, content);
-    } else if (messageType === 'whatsapp') {
-      await sendWhatsapp(subject, content);
-    }
-  res.status(200).send('Message sent and saved successfully.');
-} catch (error) {
-  console.error('Error sending and saving message:', error);
-  res.status(500).send(`Error sending and saving message: ${error.message}`);
-}
-});
 
 app.use("/api/v1/user", userRouter);
 
