d237bb20ed6c4da1e0e27d1b5356d68dbb0906b8
javascript2018-web <fiverrinbox59@gmail.com>
2024-06-08 17:06:03 +0600

    update

--- controler/communicationController.js
+++ controler/communicationController.js
@@ -3,7 +3,11 @@ const { sendEmail, sendSms, sendWhatsapp } = require('../messageUtils');
 const cloudinary = require('cloudinary').v2;
 const Message = require('../modal/Message');
 
-
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 exports.handleCommunication = async (req, res, next) => {
   res.header('Content-Type', 'application/json');
